29d2ae583547f5ad461e1529335b42bdbaa0d544
g360 User <g360user@iMacProrigliano.at.indyhall.org>
2015-01-29 16:20:20 -0500

    updates

--- hello/hello.py
+++ hello/hello.py
@@ -1,11 +1,32 @@
 import webapp2
 
 
+form = """
+<form action="/testform" method="post">
+    <input name="q">
+    <input type="submit">
+</form>
+"""
+
+
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, Udacity app world!')
+        # self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write(form)
+
+
+class TestHandler(webapp2.RequestHandler):
+    def post(self):
+        q = self.request.get('q')
+        self.response.write(q)
+
+        # To see the request headers, comment the above
+        # and uncomment the code below.
+        # self.response.headers['Content-Type'] = 'text/plain'
+        # self.response.write(self.request)
+
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/testform', TestHandler)
 ], debug=True)
