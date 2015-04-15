eff959c0a9efc328c2d4dbeb14e5e20ad4b60c8f
John Merigliano <johnmerigliano@Johns-Air.fios-router.home>
2015-04-15 08:23:30 -0400

    hw sol

--- user_accounts_security/signup-accounts/signup_account.py
+++ user_accounts_security/signup-accounts/signup_account.py
@@ -1,18 +1,34 @@
 import os
 import re
+import random
+import hashlib
+import hmac
+import string
 import webapp2
 import jinja2
+from google.appengine.ext import db
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(template_dir),
     autoescape=True)
 
+secret = 'nvSqlliCsiKCcfdsYAiTtvqbqwtDJxLSWxLgLfpZlZpKgApJMs'
 
 # regexs for validation:
 _user_re = re.compile(r'^[a-zA-Z0-9_-]{3,20}$')
 _pw_re = re.compile(r'^.{3,20}$')
 _email_re = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+
+
+def make_secure_val(s):
+    return "%s|%s" % (s, hmac.new(secret, s).hexdigest())
+
+
+def check_secure_val(h):
+    val = h.split('|')[0]
+    if h == make_secure_val(val):
+        return val
 
 
 # validation procedures
@@ -32,7 +48,7 @@ def valid_email(s):
     return _email_re.match(s)
 
 
-class Handler(webapp2.RequestHandler):
+class BlogHandler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.write(*a, **kw)
 
@@ -43,83 +59,139 @@ class Handler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
-
-class MainPage(Handler):
-
-    def get(self):
-
-        self.render(
-            "login.html",
-            err_name='',
-            err_password='',
-            err_verify='',
-            err_email='',
-            username='',
-            password='',
-            verify='',
-            email='')
-
-        username = self.request.cookies.get('username')
-
+    def set_secure_cookie(self, name, val):
+        cookie_val = make_secure_val(val)
         self.response.headers.add_header(
             'Set-Cookie',
-            'username=%s' % str(username))
+            '%s=%s; Path=/' % (name, cookie_val))
+
+    def read_secure_cookie(self, name):
+        cookie_val = self.request.cookies.get(name)
+        return cookie_val and check_secure_val(cookie_val)
+
+    # login and logout functions will go here
+
+    def initialize(self, *a, **kw):
+        webapp2.RequestHandler.initialize(self, *a, **kw)
+        uid = self.read_secure_cookie('user_id')
+        self.user = uid and User.by_id(int(uid))
+
+
+def make_salt(length=5):
+    return ''.join(random.choice(string.letters) for x in range(length))
+
+
+def make_pw_hash(name, pw, salt=None):
+    if not salt:
+        s = make_salt()
+    h = hashlib.sha256(name + pw + s).hexdigest()
+    return '%s,%s' % (h, s)
+
+
+def valid_pw(name, pw, h):
+    salt = h.split(',')[1]
+    return h == make_pw_hash(name, pw, salt)
+
+
+def users_key(group='default'):
+    return db.Key.from_path('users', group)
+
+
+class User(db.Model):
+    name = db.StringProperty(required=True)
+    pw_hash = db.StringProperty(required=True)
+    email = db.StringProperty()  # optional
+
+    # decorators
+    @classmethod
+    def by_id(cls, uid):
+        return User.get_by_id(uid, parent=users_key())
+
+    @classmethod
+    def by_name(cls, name):
+        u = User.all().filter('name=', name).get()
+        return u
+
+    @classmethod
+    def register(cls, name, pw, email=None):
+        pw_hash = make_pw_hash(name, pw)
+        return User(parent=users_key(),
+                    name=name,
+                    pw_hash=pw_hash,
+                    email=email)
+
+    @classmethod
+    def login(cls, name, pw):
+        u = cls.by_name(name)
+        if u and valid_pw(name, pw, u.pw_hash):
+            return u
+
+
+class Signup(BlogHandler):
+
+    def get(self):
+        self.render("login.html")
 
     def post(self):
+        have_error = False
+        self.user_username = self.request.get('username')
+        self.user_password = self.request.get('password')
+        self.user_verify = self.request.get('verify')
+        self.user_email = self.request.get('email')
 
-        user_username = self.request.get('username')
-        user_password = self.request.get('password')
-        user_verify = self.request.get('verify')
-        user_email = self.request.get('email')
+        params = dict(username=self.username,
+                      email=self.email)
 
-        username = valid_username(user_username)
-        password = valid_password(user_password)
-        verify = valid_verify(user_password, user_verify)
-        email = valid_email(user_email)  # email is an optional field
-        email_ok = True
+        if not valid_username(self.username):
+            params['err_name'] = "That's not a valid username."
+            have_error = True
 
-        # validate the fields and store errors
-        # populates the empty list with placeholder values
-        err = [s for s in (' ' * 8)]
+        if not valid_password(self.password):
+            params['err_password'] = "That's not a valid password."
+            have_error = True
 
-        if not username:
-            err[0] = "That's not a valid username."
+        elif self.password != self.verify:
+            params['err_verify'] = "Passwords do not match."
 
-        if not password:
-            err[1] = "That's not a valid password."
+        if not valid_email(self.email):
+            params['err_email'] = "That's not a valid email."
+            have_error = True
 
-        if not verify:
-            err[2] = "Passwords do not match."
-
-        if user_email and not email:
-            err[3] = "That's not a valid email."
-            email_ok = False
-
-        if (username and password and verify and email_ok):
-
-            self.redirect('/welcome')
-
+        if have_error:
+            self.render('login.html', **params)
         else:
-            self.render(
-                "login.html",
-                err_name=err[0],
-                err_password=err[1],
-                err_verify=err[2],
-                err_email=err[3],
-                username=user_username,
-                password='',
-                verify='',
-                email='')
+            self.done()
+
+    def done(self, *a, **kw):
+        raise NotImplementedError
+
+
+class DoneSignup(Signup):
+    def done(self):
+        self.redirect('/welcome/welcome?username=' + self.username)
+
+
+class Register(Signup):
+    def done(self):
+        # verify the user does not already exist
+        u = User.by_name(self.username)
+        if u:
+            msg = 'User already exists'
+            self.render('login.html', error_username=msg)
+        else:
+            u = User.register(self.username, self.password, self.email)
+            u.put()
+
+            self.login(u)
+            self.redirect('/welcome')
 
 
 class WelcomeHandler(webapp2.RequestHandler):
-
     def get(self):
-
         self.response.out.write("Welcome, ")
 
 # URL mapping
 application = webapp2.WSGIApplication([
-    ('/signup', MainPage),
+    ('/signup', Register),
     ('/welcome', WelcomeHandler)
 ], debug=True)
